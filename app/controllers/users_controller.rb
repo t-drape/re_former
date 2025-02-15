66cba3c01ec49343e0dbb88e3f673df74ffcfbad
t-drape <thebatreview1@gmail.com>
2025-02-14 21:48:47 -0700

    Finish Part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.new(username: params[:username], password: params[:password], email: params[:email])
+    # @user = User.new(username: params[:username], password: params[:password], email: params[:email])
     # @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
